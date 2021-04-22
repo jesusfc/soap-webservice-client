c1ac9ff4bfd269a5f2fd407576fe4596c5bb4193
Jesúsfc <jfcaraballo@gmail.com>
2021-04-22 22:51:40 +0200

    Firts commit for the project

---
+++ src/main/java/com/jesusfc/SoapWsClient/SoapWsClientApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.SoapWsClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusfc/SoapWsClient/SoapWsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.SoapWsClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
