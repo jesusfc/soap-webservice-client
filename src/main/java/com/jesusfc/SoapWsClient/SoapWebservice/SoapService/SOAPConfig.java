0b8f9abe5e4d3d3a0d7fc3c30b71199d4ee2ab74
jesusfc <jfcaraballo@gmail.com>
2021-04-29 09:10:54 +0200

    Update for new project

--- src/main/java/com/jesusfc/SoapWsClient/SoapWebservice/SoapService/SOAPConfig.java
+++ src/main/java/com/jesusfc/SoapWsClient/SoapWebservice/SoapService/SOAPConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+import javax.xml.bind.Marshaller;
+
 
 @Configuration
 public class SOAPConfig {
@@ -17,6 +19,7 @@ public class SOAPConfig {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         // this is the package name specified in the <generatePackage> specified in pom.xml
         marshaller.setContextPath("com.jesusfc.SoapWsClient.SoapWebservice.SoapSchemas");
+       // marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         return marshaller;
     }
 
